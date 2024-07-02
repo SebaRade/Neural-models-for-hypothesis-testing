9bf748e227ddcd318d09812cbc85d565e4a6dd3a
SebaRade <156301448+SebaRade@users.noreply.github.com>
2024-07-02 13:52:37 +0200

    Create asd_dataset.py

---
+++ asd_dataset.py
@@ -0,0 +1,19 @@
+import numpy as np
+from numpy import random
+
+# Introduction of errors to mimic ASD conditions
+def shuff():
+  n = .5
+  y_shuff = y.copy()
+  ix = np.random.choice([True, False], size=y_shuff.size, replace=True, p=[n, 1-n])
+  y_shuff_shuff = y_shuff[ix]
+  np.random.shuffle(y_shuff_shuff)
+  y_shuff[ix] = y_shuff_shuff
+  return y_shuff
+
+y_shuff_0 = shuff()
+y_shuff_10 = shuff()
+y_shuff_20 = shuff()
+y_shuff_30 = shuff()
+
+y_asd = np.concatenate((y_shuff_0, y_shuff_10, y_shuff_20, y_shuff_30), axis=0)
